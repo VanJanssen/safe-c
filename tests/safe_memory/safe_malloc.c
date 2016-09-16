1d00fb244b951f61ecc0ff056d1ed63654dd82c9
Erwin Janssen <erwinjanssen@outlook.com>
2016-09-17 00:14:43 +0200

    Add test for safe_malloc when passing a large size
    When passing a large value to malloc (SIZE_MAX in this case), malloc should fail and return NULL. safe_malloc should then exit and print a message.

--- tests/safe_memory/safe_malloc.c
+++ tests/safe_memory/safe_malloc.c
@@ -2,7 +2,15 @@
 
 #include "safe-c/safe_memory.h"
 
+#include <stdint.h>
+
 Test(safe_malloc, zero_expect_exit, .exit_code = 1)
 {
     safe_malloc(0);
 }
+
+Test(safe_malloc, max_expect_exit, .exit_code = 1)
+{
+    safe_malloc(SIZE_MAX);
+}
+
