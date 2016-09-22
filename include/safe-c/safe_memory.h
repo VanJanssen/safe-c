daf7135964ad39a5ff376e33fcaa76a4e311e7ff
Erwin Janssen <erwinjanssen@outlook.com>
2016-09-23 01:27:34 +0200

    `safe_realloc` function added
    Added the function `safe_realloc_function` and the macro `safe_realloc`.
    The purpose is to provide memory reallocation with strict boundary and
    error checking. The function `safe_realloc_function` requires the user to
    pass the address of the pointer to be reallocated and cast it to (void**),
    it also needs to know the name of the calling function. The macro
    `safe_realloc` performs these additional steps, so the user can call
    `safe_realloc` the same way as calling `realloc`.

--- include/safe-c/safe_memory.h
+++ include/safe-c/safe_memory.h
@@ -14,7 +14,7 @@ extern "C" {
  *      The size in bytes to be allocated. Be aware that size in an unsigned
  *      type, passing signed values can lead to conversion errors.
  *      If `size == 0`, this function will call `exit()` and print an error
- *      message indicating with function requested zero memory. You should set
+ *      message indicating which function requested zero memory. You should set
  *      your pointer to NULL yourself explicitly if you want a pointer without
  *      memory allocation.
  *
@@ -23,7 +23,7 @@ extern "C" {
  *      custom NULL termination string, or pass the __FUNCTION__ define. You
  *      can use the macro function `safe_malloc(size)`, which will automatically
  *      pass the __FUNCTION__ define as second parameter. This usage is the most
- *      convenient because the calling it will be the same as `malloc`.
+ *      convenient because calling it will be the same as `malloc`.
  *
  * @return
  *      The return value will be a pointer to the allocated memory, the memory
@@ -50,7 +50,59 @@ void* safe_malloc_function(size_t size, const char* calling_function);
  */
 void safe_free_function(void** pointer_address);
 
-#define safe_free(pointer) safe_free_function((void **) &(pointer))
+#define safe_free(pointer) safe_free_function((void**) &(pointer))
+
+/**
+* Memory reallocation with strict boundary and error checking.
+*
+* @param pointer_address
+*      The address of the pointer pointing to the memory block to be reallocated.
+*      This function changes the size of this memory block to `size` bytes. The
+*      contents will be unchanged in the range from the start of the region up
+*      to the minimum of the old and the new size.
+*      If `pointer_address == NULL`, this function will call `exit()` and print
+*      an error message containing the caller function.
+*      Unless the value pointed to be `pointer_address` is NULL, it must be a
+*      valid pointer returned by an earlier call to a memory (re)allocation
+*      function.
+*      To make it easier to call this function, the macro function
+*      `safe_realloc(pointer, size)` can be used. This macro function takes the
+*      address of the pointer, which makes calling it the same as calling
+*      `realloc`.
+*
+* @param size
+*      The new size in bytes to for the memory block pointed to by `pointer`.
+*      Be aware that size in an unsigned type, passing signed values can lead
+*      to conversion errors.
+*      If `size == 0`, this function will free the pointer and then call `exit()`
+*      and print an error message indicating which function requested zero
+*      memory. You should set your pointer to NULL yourself explicitly if you
+*      want a pointer without memory allocation.
+*      If the new size is larger than the old size, the added memory will
+*      _not_ be initialized.
+*
+* @param calling_function
+*      The name of the function calling this function. You can pass a
+*      custom NULL termination string, or pass the __FUNCTION__ define. You
+*      can use the macro function `safe_malloc(size)`, which will automatically
+*      pass the __FUNCTION__ define as second parameter. This usage is the most
+*      convenient because calling it will be the same as `realloc`.
+*
+* @return
+*      The return value will be a pointer to the reallocated memory.
+*      If the new size is larger than the old size, the added memory will _not_
+*      be initialized.
+*      If the memory block was moved, this pointer points to the new block and
+*      the previous pointer is freed and set to NULL. Otherwise this pointer
+*      is the same pointer as before.
+*      If for some reason the memory couldn't be reallocated, this function will
+*      call `exit()` and print an error message indicating the caller function
+*      and the amount of memory request.
+*/
+void* safe_realloc_function(void** pointer_address, size_t size,
+        const char* calling_function);
+
+#define safe_realloc(pointer, size) safe_realloc_function((void**) &(pointer), size, __FUNCTION__)
 
 #ifdef __cplusplus
 }
--- src/safe_memory.c
+++ src/safe_memory.c
@@ -9,8 +9,7 @@ void* safe_malloc_function(size_t size, const char* calling_function)
     {
         fprintf(stderr, "Error allocating memory: The function %s called "
                 "'safe_malloc' and requested zero memory. The pointer should "
-                "be explicitly set to NULL instead.\n",
-                calling_function);
+                "be explicitly set to NULL instead.\n", calling_function);
         exit(EXIT_FAILURE);
     }
 
@@ -34,4 +33,43 @@ void safe_free_function(void** pointer_address)
         free(*pointer_address);
         *pointer_address = NULL;
     }
+}
+
+void* safe_realloc_function(void** pointer_address, size_t size,
+        const char* calling_function)
+{
+    if (!pointer_address)
+    {
+        fprintf(stderr, "Error allocating memory: The function %s called "
+                "'safe_realloc', but the address to the pointer to be "
+                "reallocated is NULL.\n", calling_function);
+        exit(EXIT_FAILURE);
+    }
+
+    if (size == 0)
+    {
+        fprintf(stderr, "Error allocating memory: The function %s called "
+                "'safe_realloc' and requested zero memory. The pointer should "
+                "be explicitly set to NULL instead.\n", calling_function);
+        exit(EXIT_FAILURE);
+    }
+
+    void* memory = realloc(*pointer_address, size);
+    if (!memory)
+    {
+        fprintf(stderr, "Error allocating memory: The function %s called "
+                "'safe_realloc' requesting %zu bytes of memory, but an error "
+                "occurred allocating this amount of memory.\n",
+                calling_function, size);
+        exit(EXIT_FAILURE);
+    }
+
+    if (memory != *pointer_address)
+    {
+        // If the memory block is moved, relloc frees the pointer. To ensure
+        // the same behaviour as `safe_free`, we set the pointer to NULL.
+        *pointer_address = NULL;
+    }
+
+    return memory;
 }
