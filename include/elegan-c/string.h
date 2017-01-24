9d53bc300c2368ca11c5be97ce94531695a8bd46
Erwin Janssen <erwinjanssen@outlook.com>
2017-01-24 01:45:33 +0100

    New string typedefs in elegan-c/string.h
    The new string typedefs no longer have the underscore in their name
    (c_string became cstring). They also have documentation explaining their
    purpose.

--- include/elegan-c/string.h
+++ include/elegan-c/string.h
@@ -5,8 +5,31 @@
 extern "C" {
 #endif
 
-typedef char* c_string;
-typedef c_string* c_string_array;
+/**
+ * cstring
+ * 
+ * A convenient way to differentiate between a pointer to a single char and a
+ * c-string (both have the type `char *`): use `cstring` for the latter.
+ * Note that it is still a pointer and memory allocation and deallocation
+ * should still be done.
+ */
+typedef char * cstring;
+
+/**
+ * const_cstring
+ *
+ * A less verbose way to declare a constant c-string. The pointer cannot be
+ * changed and neither can the string to which it points.
+ */
+typedef const char const * const_cstring;
+
+/**
+ * cstring_array
+ * A convenient way to differentiate between a pointer to a single c-string
+ * and an array of multiple c-strings (both have the type `char **`, or
+ * `cstring *`): use `cstring_array` for the latter.'
+ */
+typedef cstring * cstring_array;
 
 #ifdef __cplusplus
 }
