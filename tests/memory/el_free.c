cc43417fd95fe774cb7fb4da559c10efdf65c34e
Erwin Janssen <erwinjanssen@outlook.com>
2017-01-30 17:15:57 +0100

    Improve unit tests for el_free functions
    - Additional test cases for `el_free_address`.
    - Verify that `pointer` is set to NULL is all tests.

--- tests/memory/el_free.c
+++ tests/memory/el_free.c
@@ -2,16 +2,34 @@
 
 #include "elegan-c/memory.h"
 
-Test(el_free_address, null_succeed) { el_free_address(NULL); }
+Test(el_free_address, address_null) { el_free_address(NULL); }
 
-Test(el_free, null_succeed)
+Test(el_free_address, address_valid_pointer_null)
 {
     void *pointer = NULL;
-    el_free_address(pointer);
+    el_free_address(&pointer);
+
+    cr_assert_null(pointer);
 }
 
-Test(el_free, free_malloced_succeed)
+Test(el_free_address, address_valid_pointer_valid)
+{
+    void *pointer = el_malloc(10);
+    el_free_address(&pointer);
+
+    cr_assert_null(pointer);
+}
+
+Test(el_free, pointer_null)
+{
+    void *pointer = NULL;
+    el_free(pointer);
+    cr_assert_null(pointer);
+}
+
+Test(el_free, pointer_valid)
 {
     void *pointer = el_malloc(50);
     el_free(pointer);
+    cr_assert_null(pointer);
 }
