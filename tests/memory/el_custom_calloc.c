10e48af056c07a9e04fe59ad19cc7a87561a39ac
Erwin Janssen <erwinjanssen@outlook.com>
2017-01-30 13:47:43 +0100

    Improve unit tests for el_calloc functions
    Remove `.exit_code = 0` in test cases. And exit code of zero is implicit
    and doesn't have to be specified.
    
    Addded three tests to `el_custom_calloc`, to verify that the error
    function is called when it should, and isn't called when no error occurs.
    
    Assigned and freed the pointer returned by `el_custom_calloc` when
    `SIZE_MAX` is passed. This prevents a memory leak in no `exit()` is
    called.
    
    Added test to verify that a negative line number in `el_unsafe_calloc`
    doesn't cause a crash.

--- tests/memory/el_custom_calloc.c
+++ tests/memory/el_custom_calloc.c
@@ -7,8 +7,11 @@
 
 bool zero_elements_function_called = false;
 bool zero_size_function_called = false;
+bool zero_both_function_called = false;
 bool max_elements_function_called = false;
 bool max_size_function_called = false;
+bool max_both_function_called = false;
+bool valid_both_function_called = false;
 
 static void zero_elements_function(int value)
 {
@@ -17,11 +20,20 @@ static void zero_elements_function(int value)
         zero_elements_function_called = true;
     }
 }
+
 static void zero_size_function(int value)
 {
     if (value == EXIT_FAILURE)
     {
         zero_size_function_called = true;
+    }
+}
+
+static void zero_both_function(int value)
+{
+    if (value == EXIT_FAILURE)
+    {
+        zero_both_function_called = true;
     }
 }
 
@@ -32,11 +44,28 @@ static void max_elements_function(int value)
         max_elements_function_called = true;
     }
 }
+
 static void max_size_function(int value)
 {
     if (value == EXIT_FAILURE)
     {
         max_size_function_called = true;
+    }
+}
+
+static void max_both_function(int value)
+{
+    if (value == EXIT_FAILURE)
+    {
+        max_both_function_called = true;
+    }
+}
+
+static void valid_both_function(int value)
+{
+    if (value == EXIT_FAILURE)
+    {
+        valid_both_function_called = true;
     }
 }
 
@@ -60,14 +89,26 @@ Test(el_custom_calloc, zero_size_triggers_function)
     cr_assert(zero_size_function_called);
 }
 
+Test(el_custom_calloc, zero_both_triggers_function)
+{
+    el_handlers handlers = el_default_handlers();
+    handlers.error_function = zero_both_function;
+
+    cr_assert_not(zero_both_function_called);
+    el_custom_calloc(0, 0, handlers);
+    cr_assert(zero_both_function_called);
+}
+
 Test(el_custom_calloc, max_elements_triggers_function)
 {
     el_handlers handlers = el_default_handlers();
     handlers.error_function = max_elements_function;
 
     cr_assert_not(max_elements_function_called);
-    el_custom_calloc(SIZE_MAX, 10, handlers);
+    void *pointer = el_custom_calloc(SIZE_MAX, 10, handlers);
     cr_assert(max_elements_function_called);
+
+    el_free(pointer);
 }
 
 Test(el_custom_calloc, max_size_triggers_function)
@@ -76,8 +117,34 @@ Test(el_custom_calloc, max_size_triggers_function)
     handlers.error_function = max_size_function;
 
     cr_assert_not(max_size_function_called);
-    el_custom_calloc(10, SIZE_MAX, handlers);
+    void *pointer = el_custom_calloc(10, SIZE_MAX, handlers);
     cr_assert(max_size_function_called);
+
+    el_free(pointer);
+}
+
+Test(el_custom_calloc, max_both_triggers_function)
+{
+    el_handlers handlers = el_default_handlers();
+    handlers.error_function = max_both_function;
+
+    cr_assert_not(max_both_function_called);
+    void *pointer = el_custom_calloc(SIZE_MAX, SIZE_MAX, handlers);
+    cr_assert(max_both_function_called);
+
+    el_free(pointer);
+}
+
+Test(el_custom_calloc, valid_both_not_trigger_function)
+{
+    el_handlers handlers = el_default_handlers();
+    handlers.error_function = valid_both_function;
+
+    cr_assert_not(valid_both_function_called);
+    void *pointer = el_custom_calloc(10, 10, handlers);
+    cr_assert_not(valid_both_function_called);
+
+    el_free(pointer);
 }
 
 Test(el_custom_calloc, handlers_out_stream_null)
@@ -109,7 +176,6 @@ Test(el_custom_calloc, handlers_error_function_null)
 
 Test(el_custom_calloc, handlers_all_null)
 {
-    el_handlers handlers = el_null_handlers();
-    void *pointer = el_custom_calloc(10, 10, handlers);
+    void *pointer = el_custom_calloc(10, 10, el_null_handlers());
     el_free(pointer);
 }
